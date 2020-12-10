20b74af795fb490d49152f067f9c2f4523d7d49e
baldo-vela <baldo.vela+github@gmail.com>
2020-12-10 11:06:45 -0600

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,24 +1,61 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  <<-SQL
+    SELECT title, year
+    FROM Books
+    WHERE series_id = 1
+    ORDER BY year;
+  SQL
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  <<-SQL
+    SELECT name, motto
+    FROM Characters
+    ORDER BY length(motto) DESC
+    LIMIT 1;
+  SQL
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  <<-SQL
+    SELECT species, count(*)
+    FROM Characters
+    GROUP BY species
+    ORDER BY count(*) DESC
+    LIMIT 1;
+  SQL
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  <<-SQL
+    SELECT Authors.name AS author_name, Subgenres.name AS subgenre_name FROM Series
+    INNER JOIN Authors
+    ON Series.author_id = Authors.id
+    INNER JOIN Subgenres
+    ON Series.subgenre_id = Subgenres.id;
+  SQL
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  <<-SQL
+    SELECT Series.title
+    FROM Series
+    INNER JOIN character_books, books, characters
+    ON Series.id = Books.series_id
+    WHERE Books.id = character_books.book_id
+    GROUP BY Characters.species HAVING Characters.species = 'human'
+    LIMIT 1;
+  SQL
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  <<-SQL
+    SELECT Characters.name, COUNT(character_books.book_id)
+    FROM Characters
+    INNER JOIN character_books
+    ON Characters.id = character_books.Character_id
+    GROUP BY Characters.name
+    ORDER BY COUNT(character_books.book_id) DESC, Characters.name ASC;
+  SQL
 end
---
+++ lib/update.sql
@@ -0,0 +1,4 @@
+-- updates the species of the last character in the characters table to "Martian"
+UPDATE Characters
+    SET species = 'Martian'
+    WHERE id = 8;
